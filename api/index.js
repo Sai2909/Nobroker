c9c66ca05e22e9bc7b26c3c00c796a93b04c2617
sai kiran <saikiran.manchala@onpassive.com>
2023-11-28 11:58:05 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(express.json());
+app.use(cors());
+app.listen(4000, () => {
+    console.log('Server is running on port 4000!');
+    }
+)
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+//middleware error sending 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
---
+++ to-let/src/pages/SignIn.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'
+
+
+const SignIn = () => {
+  const [formData, setFormData] = useState();
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  console.log("signIn",formData)
+  const handleSubmit = () => {
+    setLoading(true)
+  };
+  return (
+    <div>
+      <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="email"
+            className="border p-3 rounded-lg"
+            id="email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="border p-3 rounded-lg"
+            id="password"
+            onChange={handleChange}
+          />
+         <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p>Don't have an account?</p>
+          <Link to={"/sign-up"}>
+            <span className="text-blue-700">Sign up</span>
+          </Link>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
